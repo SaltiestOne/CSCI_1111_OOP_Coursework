5f5ebbe82f5edf5f29da80c49d82b504193e5a6f
Kayden Barlow <kayden.c.barlow@gmail.com>
2023-10-18 10:38:09 -0500

    updated 12-15 to read, as well as write

--- chapter12/Exercise12dash15.java
+++ chapter12/Exercise12dash15.java
@@ -1,5 +1,8 @@
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.random.*;
 
 public class Exercise12dash15 {
@@ -8,8 +11,44 @@ public class Exercise12dash15 {
 		
 		java.io.File file = new java.io.File("Exercise12_15.txt");
 		
-		try (
 		
+		
+		writeNumbers(file);
+		
+		int[] numbies = getNumbers(file);
+		
+		Integer[] morenumbies = primitiveToObject(numbies);
+		
+		ArrayList<Integer> result = new ArrayList<Integer>(Arrays.asList((morenumbies))); 
+		
+		java.util.Collections.sort(result);
+		
+		int lines = 0;
+		
+		for (int i = 0; i < 100; i++) {
+			
+			System.out.printf("%2d",result.get(i));
+			
+			lines++;
+			
+			if (lines >= 5) {
+				
+				System.out.println();
+				lines = 0;
+			} else {
+				
+				System.out.print(" ");
+			}
+		}
+		
+		
+
+	}
+	
+	public static void writeNumbers(java.io.File file) throws Exception {
+		
+		try (
+				
 				java.io.PrintWriter output = new java.io.PrintWriter(file);
 		
 		) {
@@ -33,8 +72,43 @@ public class Exercise12dash15 {
 			}
 			
 		}
-		
-
 	}
+	
+	public static int[]	getNumbers(java.io.File file) throws Exception {
+		
+		int[] result = new int[100];
+		
+		try (
+		
+		Scanner input = new Scanner(file);
+				
+		) {
+	
+			int count = 0;
 
+			while(input.hasNext()) {
+				
+				result[count] = input.nextInt();
+				
+				count++;
+			}	
+		
+		}
+		
+		return result;
+		
+	}
+	
+	public static Integer[]	primitiveToObject(int[] input) {
+		
+		Integer[] result = new Integer[input.length];
+		
+		for (int i = 0; i < input.length; i++) {
+			
+			result[i] = new Integer(input[i]);
+			//one day, YOU will be deprecated and marked for removal.
+		}
+		
+		return result;
+	}
 }
